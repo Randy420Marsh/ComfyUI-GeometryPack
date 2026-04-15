4bd4d43b80e57a70ebeaf2399180e7dee9388bb0
PozzettiAndrea <andrea@pozzetti.it>
2026-04-15 21:18:34 +0200

    updated to 2.61

--- __init__.py
+++ __init__.py
@@ -9,8 +9,5 @@ log.info("calling register_nodes")
 NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS = register_nodes()
 
 
-from comfy_dynamic_widgets import write_mappings
-write_mappings(NODE_CLASS_MAPPINGS, __file__)
-
 WEB_DIRECTORY = "./web"
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
